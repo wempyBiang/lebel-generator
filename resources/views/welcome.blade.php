cce749d695036e2765c69830c1c8e69050f8812e
wempyBiang <44390884+wempyBiang@users.noreply.github.com>
2026-04-03 20:22:14 +0800

    H

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,26 @@
 <html>
 <head>
     <title>Upload Excel</title>
-    
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="/import" method="POST" enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="lebel">
-        <button type="submit">Upload</button>
-    </form>
+    <h1>Print Lebel</h1>
+    <section class="section-box">
+        <h2>Regular Label</h2>
+        <form action="/import" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="file" name="lebel">
+            <button type="submit">Upload</button>
+        </form>
+    </section>
+
+    <section class="section-box">
+        <h2>Grosir Label</h2>
+        <form action="/grosir" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="file" name="lebel">
+            <button type="submit">Upload</button>
+        </form>
+    </section>
 </body>
 </html>
